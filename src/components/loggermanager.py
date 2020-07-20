1861570f6288a6ccda8583a3be39ed95af6cb585
Nilson Nieto López <nilson-sn@outlook.com>
2020-07-20 10:27:14 -0500

    notify via get

--- src/components/loggermanager.py
+++ src/components/loggermanager.py
@@ -4,7 +4,7 @@ from loguru import logger
 
 import os
 
-path = '/home/logs-rpi/logs'
+path = '/home/pi/logs-rpi/logs'
 
 logger.add(sys.stderr, format="{time} {level} {message}")
 logger.add(path, rotation='100 MB')    # Once the file is too old, it's rotated
