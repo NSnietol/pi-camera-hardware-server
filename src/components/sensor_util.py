1431ccb4b306af005b04db2009abdf492150b4ab
Nilson Nieto López <nilson-sn@outlook.com>
2020-07-26 16:56:15 -0500

    env support

--- src/components/sensor_util.py
+++ src/components/sensor_util.py
@@ -10,6 +10,8 @@ SLEEP_TIME = os.environ.get('SLEEP_TIME') if 'SLEEP_TIME' in os.environ else 1
 PIN_SENSOR = os.environ.get('PIN_SENSOR') if 'PIN_SENSOR' in os.environ else 16
 PIN_RED_LED = os.environ.get('PIN_RED_LED') if 'PIN_RED_LED' in os.environ else 10
 PIN_BLUE_LED = os.environ.get('PIN_BLUE_LED') if 'PIN_BLUE_LED' in os.environ else 8
+HOST = os.environ.get('API_SERVER_HOST') if 'API_SERVER_HOST' in os.environ else 'http://192.168.1.7:9090/'
+ACCESO_VEHICULAR_PATH = os.environ.get('ACCESO_VEHICULAR_PATH') if 'ACCESO_VEHICULAR_PATH' in os.environ else 'raspberry/api/v1/accesos/registro-acceso-vehicular'
 
 
 IO.setwarnings(False)
@@ -26,7 +28,6 @@ def check_movements():
             if(IO.input(PIN_SENSOR) == False):
                 logger.info("Obstacle detected !!")
                 take_picture()
-                notify_by_picture()
                 time.sleep(int(SLEEP_TIME))           
             time.sleep(int(SLEEP_TIME))
         except Exception as e:
@@ -48,14 +49,13 @@ def reset_status():
 
 def notify_by_picture():
     logger.info("notify_by_picture !!")
-    HOST = os.environ.get('API_SERVER_HOST') if 'API_SERVER_HOST' in os.environ else 'http://192.168.1.7:9090/raspberry/api/v1/accesos/registro-acceso-vehicular'
-    logger.info("Host {0}",HOST)
+    logger.info("Host {0}",HOST+ACCESO_VEHICULAR_PATH)
     for index in range(3):
         try:
             files = {'file': open('matricula.jpg', 'rb')}
-            response = requests.post(HOST, files=files)
+            response = requests.post(HOST+ACCESO_VEHICULAR_PATH, files=files)
             if(response.status_code == HTTPStatus.OK):
-                logger.debug("Response\n"+ str(response.content))
+                logger.debug("Response ok")
                 break
         except Exception as e:
                 logger.exception(e)
